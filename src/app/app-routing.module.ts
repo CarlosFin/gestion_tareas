ff50e2b524d775208eb3a643835585929aa06516
PixalStudioDev <115955495+PixalStudioDev@users.noreply.github.com>
2024-11-25 13:01:24 +0100

    Añadir componente primary button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrimaryButtonComponent } from './primary-button/primary-button.component'; // Importa el componente
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PrimaryButtonComponent } // Ruta predeterminada que muestra PrimaryButtonComponent
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { PrimaryButtonComponent } from './primary-button/primary-button.component';
+import { AppRoutingModule } from './app-routing.module'; // Importa el módulo de enrutamiento
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimaryButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatButtonModule,
+    AppRoutingModule // Asegúrate de importar el módulo de enrutamiento aquí
   ],
   providers: [],
   bootstrap: [AppComponent]
