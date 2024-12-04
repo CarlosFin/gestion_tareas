c534e252b7fb2876fc56a33bd066e185026447cc
CarlosFin <riv.lop.carlos@gmail.com>
2024-12-04 11:38:28 +0100

    Creacion de los componentes faltantes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CardUserComponent } from './card-user/card-user.component';
+import { FormularioUsuarioComponent } from './formulario-usuario/formulario-usuario.component';
+import { FormularioTareaComponent } from './formulario-tarea/formulario-tarea.component';
+import { BotonRadioPendientesComponent } from './boton-radio-pendientes/boton-radio-pendientes.component';
+import { BotonRadioTerminadasComponent } from './boton-radio-terminadas/boton-radio-terminadas.component';
+import { BotonRadioTodasComponent } from './boton-radio-todas/boton-radio-todas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardUserComponent,
+    FormularioUsuarioComponent,
+    FormularioTareaComponent,
+    BotonRadioPendientesComponent,
+    BotonRadioTerminadasComponent,
+    BotonRadioTodasComponent
   ],
   imports: [
     BrowserModule,
