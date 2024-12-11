1edea6ce1b5f11578d64694ab6c0dbdbba8b46ad
Rafael2026 <rafaaguilarmunoz@gmail.com>
2024-12-11 12:36:47 +0100

    Create cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { PrimaryButtonComponent } from './primary-button/primary-button.component';
-import { AppRoutingModule } from './app-routing.module'; // Importa el módulo de enrutamiento
+import { AppRoutingModule } from './app-routing.module';
+import { CardComponent } from './card/card.component'; // Importa el módulo de enrutamiento
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimaryButtonComponent
+    PrimaryButtonComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
