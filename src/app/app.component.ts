934ca663c026e1a6e3336e099a1347b56522afd2
Rafael2026 <rafaaguilarmunoz@gmail.com>
2025-01-13 13:53:04 +0100

    Pequeños cambios y mejorar la integración del modo claro y oscuro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,35 +10,6 @@ export class AppComponent {
 
   title: string = "";
 
-  /*toggleTheme() {
-
-    if (!document.getElementsByClassName('button')[0].getElementsByTagName('span')[0].classList.contains('dark')) {
-
-      document.getElementsByClassName('button')[0].getElementsByTagName('span')[0].innerHTML = 'dark_mode';
-      document.getElementsByClassName('button')[0].getElementsByTagName('span')[0].className += ' dark';
-
-      document.getElementsByTagName('nav')[0].style.backgroundColor = 'var(--color-header-oscuro)';
-      document.body.style.backgroundColor = 'var(--dark-bg)';
-      document.getElementsByTagName('h1')[0].style.color = '--claro-letra';
-
-      for (let i = 0; i < document.getElementsByTagName('a').length; i++) {
-        document.getElementsByTagName('a')[i].style.color = 'var(--white-bg)';
-      }
-
-    } else {
-      document.getElementsByClassName('button')[0].getElementsByTagName('span')[0].innerHTML = 'light_mode';
-      document.getElementsByClassName('button')[0].getElementsByTagName('span')[0].className = 'material-icons';
-
-      document.getElementsByTagName('nav')[0].style.backgroundColor = 'var(--color-header-claro)';
-      document.body.style.backgroundColor = 'var(--white-bg)';
-      document.getElementsByTagName('h1')[0].style.color = '--oscuro-letra';
-
-      for (let i = 0; i < document.getElementsByTagName('a').length; i++) {
-        document.getElementsByTagName('a')[i].style.color = 'var(--dark-bg)';
-      }
-    }
-  }*/
-
   /*
   constructor (private themeService: ModoClaroOscuroService, private renderer: Renderer2) {}
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,31 +8,64 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+
   toggleTheme() {
 
-    if (!document.getElementsByClassName('button')[0].getElementsByTagName('span')[0].classList.contains('dark')) {
+    for (let i = 0; i < document.getElementsByTagName('button').length; i++) {
 
-      document.getElementsByClassName('button')[0].getElementsByTagName('span')[0].innerHTML = 'dark_mode';
-      document.getElementsByClassName('button')[0].getElementsByTagName('span')[0].className += ' dark';
+      if (!document.getElementsByClassName('button')[i].getElementsByTagName('span')[0].classList.contains('dark')) {
 
-      document.getElementsByTagName('nav')[0].style.backgroundColor = 'var(--color-header-oscuro)';
-      document.body.style.backgroundColor = 'var(--dark-bg)';
-      document.getElementsByTagName('h1')[0].style.color = '--claro-letra';
+        document.getElementsByClassName('button')[i].getElementsByTagName('span')[0].innerHTML = 'dark_mode';
+        document.getElementsByClassName('button')[i].getElementsByTagName('span')[0].className += ' dark';
 
-      for (let i = 0; i < document.getElementsByTagName('a').length; i++) {
-        document.getElementsByTagName('a')[i].style.color = 'var(--white-bg)';
-      }
+        for (let i = 0; i < document.getElementsByTagName('nav').length; i++) {
+          document.getElementsByTagName('nav')[i].style.backgroundColor = 'var(--color-header-oscuro)';
+        }
 
-    } else {
-      document.getElementsByClassName('button')[0].getElementsByTagName('span')[0].innerHTML = 'light_mode';
-      document.getElementsByClassName('button')[0].getElementsByTagName('span')[0].className = 'material-icons';
+        document.body.style.backgroundColor = 'var(--dark-bg)';
 
-      document.getElementsByTagName('nav')[0].style.backgroundColor = 'var(--color-header-claro)';
-      document.body.style.backgroundColor = 'var(--white-bg)';
-      document.getElementsByTagName('h1')[0].style.color = '--oscuro-letra';
+        for (let i = 0; i < document.getElementsByTagName('h1').length; i++) {
+          document.getElementsByTagName('h1')[i].style.color = '--claro-letra';
+        }
 
-      for (let i = 0; i < document.getElementsByTagName('a').length; i++) {
-        document.getElementsByTagName('a')[i].style.color = 'var(--dark-bg)';
+        for (let i = 0; i < document.getElementsByTagName('a').length; i++) {
+          document.getElementsByTagName('a')[i].style.color = 'var(--white-bg)';
+        }
+
+        for (let i = 0; i < document.getElementsByTagName("div").length; i++) {
+
+          if (document.getElementsByTagName("div")[i].id === "infoUsuario") {
+            document.getElementsByTagName("div")[i].style.backgroundColor = "var(--card-user-oscuro)";
+            document.getElementsByTagName("div")[i].style.color = "var(--white-bg)";
+          }
+        }
+
+      } else {
+
+        document.getElementsByClassName('button')[i].getElementsByTagName('span')[0].innerHTML = 'light_mode';
+        document.getElementsByClassName('button')[i].getElementsByTagName('span')[0].className = 'material-icons';
+
+        for (let i = 0; i < document.getElementsByTagName('nav').length; i++) {
+          document.getElementsByTagName('nav')[i].style.backgroundColor = 'var(--color-header-claro)';
+        }
+
+        document.body.style.backgroundColor = 'var(--white-bg)';
+
+        for (let i = 0; i < document.getElementsByTagName('h1').length; i++) {
+          document.getElementsByTagName('h1')[i].style.color = '--oscuro-letra';
+        }
+
+        for (let i = 0; i < document.getElementsByTagName('a').length; i++) {
+          document.getElementsByTagName('a')[i].style.color = 'var(--dark-bg)';
+        }
+
+        for (let i = 0; i < document.getElementsByTagName("div").length; i++) {
+
+          if (document.getElementsByTagName("div")[i].id === "infoUsuario") {
+            document.getElementsByTagName("div")[i].style.backgroundColor = "var(--card-user-claro)";
+            document.getElementsByTagName("div")[i].style.color = "var(--dark-bg)";
+          }
+        }
       }
     }
   }
